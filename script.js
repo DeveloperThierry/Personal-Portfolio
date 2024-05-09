44e2b82bc38ade9a419c52db83d5baf54ad2b83d
School Account Thierry <151109030+SchoolAccount-T@users.noreply.github.com>
2024-05-09 03:30:07 +0000

    Modified CSS & JavaScript

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let menuIcon = document.querySelector('menu-icon');
+/*let menuIcon = document.querySelector('menu-icon');
 let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-/*window.onscroll = () => {
+window.onscroll = () => {
   sections.forEach{sec => {
     let top = window.scrollY;
     let offset = sec.offsetTop -150;
