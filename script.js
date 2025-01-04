bdb917b946b6ae7854845629230c939c15561639
Thierry <151109030+DeveloperThierry@users.noreply.github.com>
2025-01-04 04:47:20 +0000

    Added Loader

--- script.js
+++ script.js
@@ -23,3 +23,28 @@ menuIcon.addEventListener('click', () => {
   navbar.classList.toggle('active');
   menuIcon.classList.toggle('bx-x');  
 });
+
+
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyuDEvYcllKjkPkvCW2r5nTvGpbqOx9a88oR52C5QwA0eV4vLJmbuYhmGGstaux31sB/exec';
+const form = document.forms['contact-form'];
+const loadingMessage = document.getElementById('loading-message');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  loadingMessage.style.display = 'block';
+
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => alert("Thank you! Form is submitted"))
+    .then(() => {
+      loadingMessage.style.display = 'none';
+      window.location.reload();
+    })
+    .catch(error => {
+      console.error('Error!', error.message);
+      loadingMessage.style.display = 'none';
+    });
+  
+});
